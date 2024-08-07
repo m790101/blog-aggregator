23f6c7e4f2e86e96834b91184530366ee0029deb
m790101 <m790101@hotmail.com>
2024-08-07 22:40:31 +0800

    add sequal

---
+++ sql/scheme/001_number_name.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name varchar(255) NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
